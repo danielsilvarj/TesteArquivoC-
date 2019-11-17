fabcd462195fa38d3d2e836ba7c80a347791bcf0
danielsilvarj <daniel.cg.rio@gmail.com>
2019-11-17 00:41:17 -0300

    Enviando Projeto

---
+++ MyClassesTest/FileProcesseTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcesseTest
+    {
+
+        private const string BAD_FILE_NAME = @"C:\BadFileName.bat";
+        private string _GoodFileName;
+
+        public TestContext TestContext { get; set; }
+
+
+        #region Test Initialize e Cleanup
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (TestContext.TestName == "FileNameDoesExists")
+            {
+                if (string.IsNullOrEmpty(_GoodFileName))
+                {
+                    SetGoodFileName();
+                    TestContext.WriteLine($"Creating File: {_GoodFileName}");
+                    File.AppendAllText(_GoodFileName, $"Some Text: {_GoodFileName}");
+                }
+            }
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (TestContext.TestName == "FileNameDoesExists")
+            {
+                if (!string.IsNullOrEmpty(_GoodFileName))
+                {
+                    TestContext.WriteLine($"Testing File: {_GoodFileName}");
+                    File.Delete(_GoodFileName);
+
+                }
+            }
+        }
+
+        #endregion
+
+        [TestMethod]
+        [Description("Check to see if a file does exsist")]
+        [Owner("Daniel")]
+        [Priority(0)]
+        [TestCategory("noException")]
+        public void FileNameDoesExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            TestContext.WriteLine($"Testing File: {_GoodFileName}");
+            fromCall = fp.FileExists(_GoodFileName);
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        [Description("Check to see if a file not does exsist")]
+        [Owner("Daniel")]
+        [Priority(0)]
+        [TestCategory("noException")]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(BAD_FILE_NAME);
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Owner("Zé")]
+        [Priority(1)]
+        [TestCategory("Exception")]
+        public void FileNameNullorEmpty_ThrowsArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        [Owner("Zé")]
+        [Priority(1)]
+        [TestCategory("Exception")]
+        public void FileNameNullorEmpty_ThrowsArgumentNullException_UsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentException)
+            {
+                //The test was a Success.
+                return;
+            }
+
+            Assert.Fail("Fail Exepected");
+        }
+
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+
+        private const string FILE_NAME = @"FileToDeploy.txt";
+
+        [TestMethod]
+        [Owner("Daniel")]
+        [DeploymentItem(FILE_NAME)]
+        public void FileNameDoesExistsUsingDeploymentItem()
+        {
+            FileProcess fp = new FileProcess();
+            string fileName;
+            bool fromCall;
+
+            fileName = $@"{TestContext.DeploymentDirectory}\{FILE_NAME}";
+            TestContext.WriteLine($"Checking File: {fileName}");
+            fromCall = fp.FileExists(fileName);
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        [Timeout(3100)]
+        [Owner("Zé")]
+        public void SimulateTimeout()
+        {
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}
---
+++ MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb526488-fe50-41fe-a8d8-d44e7bf87a39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
